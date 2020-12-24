508de70a0271c33d9eca559e275e7b1219ed08cf
Tobi <71779870+tobi2021lol@users.noreply.github.com>
2020-12-24 06:25:08 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author tobi2021lol
+// +------------------------------------------------------------------------+
+// | p2pSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2021 p2pSound. All rights reserved.
+// +------------------------------------------------------------------------+
+header("Location: ./install");
+exit();
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "";
+
+// Site URL
+$site_url = ""; // e.g (http://example.com.br)
+
+$developer_mode = 0;
+
+// code
+$purchase_code = ""; // code
+?>
